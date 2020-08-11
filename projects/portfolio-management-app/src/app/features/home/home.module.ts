6d27a4116ab02a5e8c96799acabac685b97988b5
Bhushan Sharma <bhushan8757@gmail.com>
2020-08-11 18:17:16 +0530

    remove stock table

--- projects/portfolio-management-app/src/app/features/home/home.module.ts
+++ projects/portfolio-management-app/src/app/features/home/home.module.ts
@@ -3,14 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { StockDataTableComponent } from './stock-data-table/stock-data-table.component';
-
 
 @NgModule({
-  declarations: [HomeComponent, StockDataTableComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
